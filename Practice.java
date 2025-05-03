1c0a3eb9d1ce2b4fee25cd5a2d68c0e09dc5f47b
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2025-05-03 23:29:26 +0200

    random

--- Practice.java
+++ Practice.java
@@ -1,6 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Practice {
-	public static void main(String[] args) {
-		String test = "This is a test";
-		System.out.printf("%s\n", test);
-	}
+  public static void main(String[] args){
+    // Your code here:
+   try {
+		System.out.println("Step 1");
+		int result = 10 / 0;
+		System.out.println("Step2");
+   } catch (ArithmeticException Name) {
+		System.out.println("Error" + Name.getMessage());
+   }
+  }
 }
